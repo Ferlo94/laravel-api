f8e3a8859a572a1ee8bd500fdc7d02a64514ad65
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-07-26 20:55:50 +0200

    seeder post

--- database/migrations/2022_07_26_183638_add_column_user_id_posts_table.php
+++ database/migrations/2022_07_26_183638_add_column_user_id_posts_table.php
@@ -14,7 +14,7 @@ class AddColumnUserIdPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->foreignId('user_id')->nullable()->after('category_id')->constrained()->onDelete('cascade');
         });
     }
 
@@ -26,7 +26,8 @@ class AddColumnUserIdPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
         });
     }
 }
